8329f83c34c0fd7f2fff6b3ff66f07f46ddfb540
Stefan Wagner <swagner@heuristiclab.com>
2019-08-20 07:32:02 +0200

    exploration of different solutions

---
+++ src/TestPluginA/A.cs
@@ -0,0 +1,12 @@
+﻿using HEAL.Bricks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPluginA {
+  public class A {
+    public override string ToString() {
+      return "A";
+    }
+  }
+}
---
+++ src/TestPluginB/B.cs
@@ -0,0 +1,13 @@
+﻿using HEAL.Bricks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestPluginA;
+
+namespace TestPluginB {
+  public class B : A {
+    public override string ToString() {
+      return base.ToString() + " - B";
+    }
+  }
+}
