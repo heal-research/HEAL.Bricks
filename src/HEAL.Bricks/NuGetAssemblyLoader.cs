af06694b1fca2bfd95d4fa361c105238d5b2b7a6
Stefan Wagner <swagner@heuristiclab.com>
2020-01-05 12:39:48 +0100

    Added NuGetAssemblyLoader

---
+++ src/HEAL.Bricks/NuGetAssemblyLoader.cs
@@ -0,0 +1,60 @@
+﻿using NuGet.Frameworks;
+using NuGet.Packaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Loader;
+using System.Runtime.Versioning;
+
+namespace HEAL.Bricks {
+  public sealed class NuGetAssemblyLoader {
+    public static void Load() {
+      string applicationPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+      foreach (string packagePath in Directory.GetFiles(applicationPath, "*.nupkg")) {
+        string symbolsPackagePath = Path.ChangeExtension(packagePath, "snupkg");
+
+        using (PackageArchiveReader packageReader = new PackageArchiveReader(packagePath)) {
+          NuspecReader nuspecReader = new NuspecReader(packageReader.GetNuspec());
+          Console.WriteLine($"Loading assemblies from package {nuspecReader.GetIdentity().ToString()}:");
+
+          var files = NuGetFrameworkUtility.GetNearest(packageReader.GetReferenceItems(), GetCurrentFramework()).Items;
+          foreach (string dllFile in files) {
+            try {
+              using (Stream assemblyStream = packageReader.GetStream(dllFile))
+              using (MemoryStream decompressedAssemblyStream = new MemoryStream()) {
+                assemblyStream.CopyTo(decompressedAssemblyStream);
+                decompressedAssemblyStream.Position = 0;
+
+                if (File.Exists(symbolsPackagePath)) {
+                  string symbolsFile = Path.ChangeExtension(dllFile, "pdb");
+                  using (PackageArchiveReader symbolsPackageReader = new PackageArchiveReader(symbolsPackagePath))
+                  using (Stream symbolsStream = symbolsPackageReader.GetStream(symbolsFile))
+                  using (MemoryStream decompressedSymbolsStream = new MemoryStream()) {
+                    symbolsStream.CopyTo(decompressedSymbolsStream);
+                    decompressedSymbolsStream.Position = 0;
+                    Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(decompressedAssemblyStream, decompressedSymbolsStream);
+                    Console.WriteLine($"Loaded assembly and symbols {assembly.FullName}.");
+                  }
+                } else {
+                  Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(decompressedAssemblyStream);
+                  Console.WriteLine($"Loaded assembly {assembly.FullName}.");
+                }
+              }
+            }
+            catch (Exception e) {
+              Console.WriteLine($"Loading assembly {dllFile} failed: {e.Message}");
+            }
+          }
+        }
+        Console.WriteLine();
+      }
+    }
+
+    private static NuGetFramework GetCurrentFramework() {
+      string frameworkName = Assembly.GetEntryAssembly().GetCustomAttribute<TargetFrameworkAttribute>().FrameworkName;
+      return NuGetFramework.ParseFrameworkName(frameworkName, DefaultFrameworkNameProvider.Instance);
+    }
+  }
+}
