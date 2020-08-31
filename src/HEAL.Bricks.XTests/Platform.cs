f8e7e882bad067d2fce4364c649280e05f973fd3
Stefan Wagner <swagner@heuristiclab.com>
2020-09-01 00:50:36 +0200

    Add missing files

---
+++ src/HEAL.Bricks.XTests/Platform.cs
@@ -0,0 +1,16 @@
+#region License Information
+/*
+ * This file is part of HEAL.Bricks which is licensed under the MIT license.
+ * See the LICENSE file in the project root for more information.
+ */
+#endregion
+
+namespace HEAL.Bricks.XTests {
+  public enum Platform {
+    Unknown,
+    Windows,
+    Linux,
+    OSX,
+    FreeBSD
+  }
+}
